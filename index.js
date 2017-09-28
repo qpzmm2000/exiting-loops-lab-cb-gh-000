f1062f4201385ab1c8ed82ebf23be06483c335bd
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 09:15:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function breakOut(array, changeValue, stopValue)
+{
+  for(var i = 0; i < array.length; i++)
+  {
+    if(changeValue == stopValue) break;
+    array[i] = changeValue;
+
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue)
+{
+  for(var i = 0; i < array.length; i++)
+  {
+    if(changeValue == stopValue) continue;
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function findBy(array, findFn)
+{
+  for(var i = 0; i < array.length; i++)
+  {
+    if(array[i] == findFn) return array[i];
+  }
+  return null;
+}
