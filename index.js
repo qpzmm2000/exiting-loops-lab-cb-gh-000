01c8277c825234cb116b23d46c8e86787fc7eb81
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 09:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function breakOut(array, changeValue, stopValue)
 {
   for(var i = 0; i < array.length; i++)
   {
-    if(changeValue == stopValue) break;
+    if(changeValue >= stopValue) break;
     array[i] = changeValue;
 
   }
